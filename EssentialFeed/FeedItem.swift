2788a7153bb9c15731c895384930f663043bff1d
Maaz <maaazsurti@gmail.com>
2024-08-03 19:18:00 +0530

    add feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Maaz Surti on 03/08/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
